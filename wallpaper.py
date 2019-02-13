73cbc2d7796fad1833cae813c8c24fd29d27177a
alex <alex@localhost>
2019-02-13 21:20:45 +0300

    разбил на простые выражения с отдельным именем #2

--- wallpaper.py
+++ wallpaper.py
@@ -1,8 +1,11 @@
-def wallpaper(lenght_room,width_room,height_room,width_wall,lenght_rollon):
+def wallpaper(lenght_room, width_room, height_room, width_wall, lenght_rollon):
     """
     >>> wallpaper(6.5,5.4,2.3,1.2,10,)
     5
     """
     import math
-    result = math.ceil((math.ceil(((lenght_room + width_room) * 2) / width_wall)) / (math.floor(lenght_rollon / (height_room + 0.1))))
+    k = 0.1  # запас длины рулона
+    perimeter_room = (lenght_room + width_room) * 2
+    rollon = math.floor(lenght_rollon / (height_room + k))
+    result = math.ceil((math.ceil(perimeter_room / width_wall)) / rollon)
     return result
