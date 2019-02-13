f28a3307fd051d355ef1d0796dab86213fa3d3f7
alex <alex@localhost>
2019-02-13 21:00:14 +0300

    Fixed words #1

--- wallpaper.py
+++ wallpaper.py
@@ -1,8 +1,8 @@
-def walleper(lenght_room,wight_room,height_room,wight_wall,lenght_rollon):
+def wallpaper(lenght_room,width_room,height_room,width_wall,lenght_rollon):
     """
-    >>> walleper(6.5,5.4,2.3,1.2,10,)
+    >>> wallpaper(6.5,5.4,2.3,1.2,10,)
     5
     """
     import math
-    result = math.ceil((math.ceil(((lenght_room + wight_room) * 2) / wight_wall)) / (math.floor(lenght_rollon / (height_room + 0.1))))
+    result = math.ceil((math.ceil(((lenght_room + width_room) * 2) / width_wall)) / (math.floor(lenght_rollon / (height_room + 0.1))))
     return result
